75ae38a13fd2185c587814f11703d6c4d8703ce8
batleforc <maxleriche.60@gmail.Com>
2018-12-18 12:57:29 +0100

    Add files via upload

---
+++ pivot.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+}
